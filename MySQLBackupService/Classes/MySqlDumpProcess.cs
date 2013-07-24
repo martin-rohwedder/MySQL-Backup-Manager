355c3269905294d4e24cc1d5758ce7b468ff284e
Martin Rohwedder <m.rohwedder@gmail.com>
2013-07-25 01:23:46 +0200

    Find the MySQL Installation Path from the Registry function added

--- MySQLBackupService/Classes/MySqlDumpProcess.cs
+++ MySQLBackupService/Classes/MySqlDumpProcess.cs
@@ -1,4 +1,5 @@
-﻿using Quartz;
+﻿using Microsoft.Win32;
+using Quartz;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -69,6 +70,7 @@ namespace MySQLBackupService.Classes
                         this.databaseName = node["DatabaseName"].InnerText;
 
                         ProcessStartInfo psi = new ProcessStartInfo();
+                        psi.WorkingDirectory = this.RetrieveMySQLInstallationBinPath();
                         psi.FileName = "mysqldump";
                         psi.RedirectStandardInput = false;
                         psi.RedirectStandardOutput = true;
@@ -97,6 +99,24 @@ namespace MySQLBackupService.Classes
                     process.WaitForExit();
                 }
             }
+        }
+
+        /**
+         * Lookup the MySQL Installation Bin path from the registry
+         */
+        private string RetrieveMySQLInstallationBinPath()
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\MySQL AB");
+            foreach (string subkey in registryKey.GetSubKeyNames())
+            {
+                if (subkey.ToLower().Contains("mysql server"))
+                {
+                    RegistryKey myKey = registryKey.OpenSubKey(subkey);
+                    string location = (string)myKey.GetValue("Location");
+                    return location + @"bin\";
+                }
+            }
+            return null;
         }
 
         /**
