82a61787c459ad674d42c64cadfa5c2e25f937a8
Martin Rohwedder <m.rohwedder@gmail.com>
2013-08-11 13:33:32 +0200

    Application Details Page added

---
+++ MySQLBackupManager/Pages/AppDetailsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySQLBackupManager.Pages
+{
+    /// <summary>
+    /// Interaction logic for AppDetailsPage.xaml
+    /// </summary>
+    public partial class AppDetailsPage : Page
+    {
+        public AppDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.ToString());
+        }
+    }
+}
